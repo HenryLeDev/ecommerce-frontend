62a0fd8e2105d2b6ae892b20796806cec36484c0
s0570716 <s0570716@htw-berlin.de>
2022-06-14 13:43:25 +0200

    proxy changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(
-      createProxyMiddleware(["/api", , "/otherApi"], { target: "http://localhost:5000" })
-    );
-  };
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+};
