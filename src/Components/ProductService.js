110917db52506d4345d3c10c5795ce03892431dc
s0570716 <s0570716@htw-berlin.de>
2022-06-15 11:28:54 +0200

    rest url change

--- src/Components/ProductService.js
+++ src/Components/ProductService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const PRODUCTS_REST_API_URL = 'http://localhost:8080/api/v2/products';
+const PRODUCTS_REST_API_URL = 'https://ecommerce-app-s0570716.herokuapp.com/api/v2/products';
 
 class ProductService {
 
